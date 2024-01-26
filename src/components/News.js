82fa31cc83299d72459e2ef5ea34317693efe091
Nizam6239 <nizammalik6239@gmail.com>
2024-01-26 23:20:43 +0530

    Adding top scroll bar

--- src/components/News.js
+++ src/components/News.js
@@ -30,14 +30,18 @@ export class News extends Component {
     )} -NewsApp`;
   }
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7bf5dbc155bc458db3802d32b6b9a5ce&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updateNews();
